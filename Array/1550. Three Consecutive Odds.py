9b528c712dd783658e9287858b7413285d55b014
Roni Dhara <bhupaldhara92@gmail.com>
2025-05-11 07:54:22 +0530

    Create 1550. Three Consecutive Odds
    Easy

---
+++ Array/1550. Three Consecutive Odds.py
@@ -0,0 +1,12 @@
+class Solution:
+    def threeConsecutiveOdds(self, arr: List[int]) -> bool:
+        odd_count = 0
+        for num in arr:
+            if num % 2 == 1:
+                odd_count += 1
+                if odd_count == 3:
+                    return True
+            else:
+                odd_count = 0
+
+        return False

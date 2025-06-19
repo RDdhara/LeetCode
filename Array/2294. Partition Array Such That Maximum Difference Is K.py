d9d015915d477fd811212e23de0c9f874e0b420e
Roni Dhara <bhupaldhara92@gmail.com>
2025-06-19 12:40:00 +0530

    Create 2294. Partition Array Such That Maximum Difference Is K
    Medium

---
+++ Array/2294. Partition Array Such That Maximum Difference Is K.py
@@ -0,0 +1,12 @@
+class Solution:
+    def partitionArray(self, nums: List[int], k: int) -> int:
+        nums.sort()
+        count = 1
+        min_val = nums[0]
+
+        for i in range(len(nums)):
+            if nums[i]  - min_val > k:
+                count += 1
+                min_val = nums[i]
+        
+        return count

5fc40b12ae89158ee705c49c6023c079b1cbb93c
Roni Dhara <bhupaldhara92@gmail.com>
2026-03-08 17:17:18 +0530

    Implement findDifferentBinaryString method
    Medium

---
+++ String/1980. Find Unique Binary String.py
@@ -0,0 +1,8 @@
+class Solution:
+    def findDifferentBinaryString(self, nums: List[str]) -> str:
+        result = ""
+
+        for i in range(len(nums)):
+            result += "0" if nums[i][i] == "1" else "1"
+        
+        return result

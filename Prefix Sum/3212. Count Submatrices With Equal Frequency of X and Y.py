4e55aeeac1252bac285170f6373041704c860d34
Roni Dhara <bhupaldhara92@gmail.com>
2026-03-19 10:23:53 +0530

    Implement cumulative sum for X and Y in grid
    Medium

--- Prefix Sum/3212. Count Submatrices With Equal Frequency of X and Y.py
+++ Prefix Sum/3212. Count Submatrices With Equal Frequency of X and Y.py
@@ -34,3 +34,34 @@ class Solution:
                     count += 1
         
         return count
+
+"""
+m = len(grid)
+        n = len(grid[0])
+
+        cum_sum_x = [[0] * n for _ in range(m)]
+        cum_sum_y = [[0] * n for _ in range(m)]
+        
+        count = 0
+
+        for i in range(m):
+            for j in range(n):
+                cum_sum_x[i][j] = (grid[i][j] == 'X')
+                cum_sum_y[i][j] = (grid[i][j] == 'Y')
+
+                if i-1 >= 0:
+                    cum_sum_x[i][j] += cum_sum_x[i-1][j]
+                    cum_sum_y[i][j] += cum_sum_y[i-1][j]
+                if j-1 >= 0:
+                    cum_sum_x[i][j] += cum_sum_x[i][j-1]
+                    cum_sum_y[i][j] += cum_sum_y[i][j-1]
+                if i-1 >= 0 and j-1 >= 0:
+                    cum_sum_x[i][j] -= cum_sum_x[i-1][j-1]
+                    cum_sum_y[i][j] -= cum_sum_y[i-1][j-1]
+
+
+                if cum_sum_x[i][j] > 0 and cum_sum_x[i][j] == cum_sum_y[i][j]:
+                    count += 1
+        
+        return count
+"""

29ca542691fa93f6f55378d397a4bda7186ecabd
Roni Dhara <bhupaldhara92@gmail.com>
2026-03-19 10:12:51 +0530

    Create 3212. Count Submatrices With Equal Frequency of X and Y.py
    Medium

---
+++ Prefix Sum/3212. Count Submatrices With Equal Frequency of X and Y.py
@@ -0,0 +1,36 @@
+class Solution:
+    def numberOfSubmatrices(self, grid: List[List[str]]) -> int:
+        m = len(grid)
+        n = len(grid[0])
+
+        cum_sum_x = [[0] * n for _ in range(m)]
+        cum_sum_y = [[0] * n for _ in range(m)]
+        
+        count = 0
+
+        for i in range(m):
+            for j in range(n):
+                # X prefix sum
+                if i-1 >= 0:
+                    cum_sum_x[i][j] += cum_sum_x[i-1][j]
+                if j-1 >= 0:
+                    cum_sum_x[i][j] += cum_sum_x[i][j-1]
+                if i-1 >= 0 and j-1 >= 0:
+                    cum_sum_x[i][j] -= cum_sum_x[i-1][j-1]
+                if grid[i][j] == 'X':
+                    cum_sum_x[i][j] += 1
+
+                 # Y prefix sum
+                if i-1 >= 0:
+                    cum_sum_y[i][j] += cum_sum_y[i-1][j]
+                if j-1 >= 0:
+                    cum_sum_y[i][j] += cum_sum_y[i][j-1]
+                if i-1 >= 0 and j-1 >= 0:
+                    cum_sum_y[i][j] -= cum_sum_y[i-1][j-1]
+                if grid[i][j] == 'Y':
+                    cum_sum_y[i][j] += 1
+
+                if cum_sum_x[i][j] > 0 and cum_sum_x[i][j] == cum_sum_y[i][j]:
+                    count += 1
+        
+        return count
